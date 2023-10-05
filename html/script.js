8c32755cd9f65a874476013e640539708644a379
André <146885341+arm332@users.noreply.github.com>
2023-10-05 15:32:32 -0300

    Trying different dialog methods

--- html/script.js
+++ html/script.js
@@ -3,37 +3,59 @@
 // Window events.
 
 self.addEventListener("DOMContentLoaded", function(event) {
-	console.log("self.DOMContentLoaded");
+	console.log("window.DOMContentLoaded");
+	setMessage("OK");
+});
+
+self.addEventListener("offline", function(event) {
+	console.log("window.offline");
+	setMessage("window.offline");
+});
+
+self.addEventListener("online", function(event) {
+	console.log("window.online");
+	setMessage("window.online");
+});
+
+// Document events.
+
+document.addEventListener("keydown", function(event) {
+	console.log("document.keydown");
+
+	if (event.key == "Escape") {
+		if (message.open) {
+			message.close();
+		}
+	}
 });
 
 // Sign up form events.
-// Form element value property is a Safe Sink.
-// See <https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#safe-sinks>
 
 signup.addEventListener("submit", async function(event) {
 	console.log("signup.submit");
 	event.preventDefault();
 
+	event.submitter.disabled = true;
+
 	const username = this.username.value.trim();
 	const password = this.password.value.trim();
 	const confirmation = this.confirmation.value.trim();
-	this.send.disabled = true;
 
 	if (!username.match(/[\w\-.]{8,}/)) {
 		setMessage("Username invalid.");
-		this.send.disabled = null;
+		event.submitter.disabled = null;
 		return;
 	}
 
 	if (!password.match(/[\w\x20-\x40]{8,}/)) {
 		setMessage("Password invalid.");
-		this.send.disabled = null;
+		event.submitter.disabled = null;
 		return;
 	}
 
 	if (password !== confirmation) {
 		setMessage("Password confirmation incorrect.");
-		this.send.disabled = null;
+		event.submitter.disabled = null;
 		return;
 	}
 
@@ -42,16 +64,19 @@ signup.addEventListener("submit", async function(event) {
 
 	if (json.error) {
 		setMessage(json.error);
-		this.send.disabled = null;
+		event.submitter.disabled = null;
 		return;
 	}
+
+	// HTMLInputElement value property is a Safe Sink.
+	// See <https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#safe-sinks>
 
 	this.username.value = json.username;
 	this.password.value = json.username;
 	this.confirmation.value = json.username;
-	this.send.disabled = null;
 
 	setMessage("ID: " + json.id);
+	event.submitter.disabled = null;
 });
 
 // Messsage dialog events.
@@ -63,38 +88,38 @@ message.addEventListener("click", function(event) {
 });
 
 // Message dialog functions.
-// Node textContent property is a Safe Sink.
-// See <https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#safe-sinks>
 
-function setMessage(textContent = "") {
+function setMessage(textContent = null) {
 	console.log("setMessage");
-	message.textContent = textContent;
-	message.showModal();
+
+	// Node textContent property is a Safe Sink.
+	// See <https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#safe-sinks>
+
+	if (textContent) {
+		message.textContent = textContent;
+		message.show();
+	}
+	else {
+		message.close();
+	}
 }
 
 // Utils.
 
-function fetchData(url, data = {}) {
+async function fetchData(url, data = null) {
 	console.log("fetchData");
 
-	return fetch(url,
-		{
-			method: "POST",
-			mode: "no-cors",
-			cache: "no-cache",
-			credentials: "same-origin",
-			headers: {"Content-Type": "application/json"},
-			redirect: "error",
-			referrerPolicy: "no-referrer",
-			body: JSON.stringify(data)
-		}
-	)
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(json) {
-		return json;
-	});
+	// credentials defaults to "same-origin"
+	// See <https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch>.
+
+	const response = await fetch(url, {method: "POST", mode: "no-cors",
+		cache: "no-cache", headers: {"Content-Type": "application/json"},
+		redirect: "error", referrerPolicy: "no-referrer",
+		body: JSON.stringify(data)});
+
+	const json = response.json();
+
+	return json;
 }
 
 //~ function sanitizeData(text) {
